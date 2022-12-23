459bac4d50cc405059a4643c07c4766aa54565d5
Hubert <hhuamanid@unsa.edu.pe>
2022-12-22 19:18:09 -0500

    Se modifico oscilador

--- Assets/Scripts/Oscilador.cs
+++ Assets/Scripts/Oscilador.cs
@@ -2,31 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[DisallowMultipleComponent] // atributo para bloquear poner un componente dos veces
-//[ExecuteInEditMode] atributo para ejecutar el script en modo edicion, muy util
+[DisallowMultipleComponent]
 
 public class Oscilador : MonoBehaviour
 {
-    [SerializeField] Vector3 posInicial;
-    [SerializeField] Vector3 dirDesplazamiento;
-    [SerializeField] [Range(0,1)] float desplazamiento;
-    [SerializeField] float periodo = 1;
+    [SerializeField] Vector3 motion;
+    [SerializeField] float period = 1;
+    Vector3 initialPosition;
 
-    // Start is called before the first frame update
     void Start()
     {
-        posInicial = transform.position;
+        initialPosition = transform.position;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        float ciclos = Time.time / periodo;
-        float tau = Mathf.PI * 2;
-        float funcionSeno = Mathf.Sin(tau * ciclos);
-        desplazamiento = funcionSeno/2 + 0.5f;
-
-        //Desplazamiento se tiene que mover de forma automatica entre 0 y 1
-        transform.position = posInicial + (dirDesplazamiento * desplazamiento);
+        float interval = 0;
+        if(period != 0)
+            interval = Mathf.Sin(Mathf.PI * 2 * Time.time / period) / 2 + 0.5f;
+        transform.position = initialPosition + motion * interval;
     }
 }
